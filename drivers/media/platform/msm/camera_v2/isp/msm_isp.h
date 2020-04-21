RaphielGang/bash_kernel_msm8953

mido: Revert "msm: isp: Add support for VFE resolution limit"
This reverts commit ebdc70c8efc13e523fcc42bb0a582f0f0febafdc.

Signed-off-by: Albert I <krascgq@outlook.co.id>

drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -772,7 +772,9 @@ struct vfe_device {
 	uint32_t is_split;
 	uint32_t dual_vfe_enable;
 	unsigned long page_fault_addr;
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 	uint32_t vfe_hw_limit;
+#endif
 
 	/* Debug variables */
 	int dump_reg;

drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
@@ -2586,8 +2586,10 @@ int msm_vfe47_enable_regulators(struct vfe_device *vfe_dev, int enable)
 int msm_vfe47_get_platform_data(struct vfe_device *vfe_dev)
 {
 	int rc = 0;
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 	void __iomem *vfe_fuse_base;
 	uint32_t vfe_fuse_base_size;
+#endif
 
 	vfe_dev->vfe_base = msm_camera_get_reg_base(vfe_dev->pdev, "vfe", 0);
 	if (!vfe_dev->vfe_base)
@@ -2612,6 +2614,7 @@ int msm_vfe47_get_platform_data(struct vfe_device *vfe_dev)
 		rc = -ENOMEM;
 		goto get_res_fail;
 	}
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 	vfe_dev->vfe_hw_limit = 0;
 	vfe_fuse_base = msm_camera_get_reg_base(vfe_dev->pdev,
 					"vfe_fuse", 0);
@@ -2624,6 +2627,7 @@ int msm_vfe47_get_platform_data(struct vfe_device *vfe_dev)
 		msm_camera_put_reg_base(vfe_dev->pdev, vfe_fuse_base,
 				"vfe_fuse", 0);
 	}
+#endif
 	rc = vfe_dev->hw_info->vfe_ops.platform_ops.get_regulators(vfe_dev);
 	if (rc)
 		goto get_regulator_fail;

include/uapi/media/msmb_isp.h
@@ -24,7 +24,9 @@
 
 #define ISP_STATS_STREAM_BIT  0x80000000
 
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 #define VFE_HW_LIMIT 1
+#endif
 
 struct msm_vfe_cfg_cmd_list;
 
@@ -502,7 +504,9 @@ enum msm_vfe_reg_cfg_type {
 	VFE_HW_UPDATE_UNLOCK,
 	SET_WM_UB_SIZE,
 	SET_UB_POLICY,
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 	GET_VFE_HW_LIMIT,
+#endif
 };
 
 struct msm_vfe_cfg_cmd2 {
@@ -959,7 +963,9 @@ enum msm_isp_ioctl_cmd_code {
 	MSM_ISP_FETCH_ENG_MULTI_PASS_START,
 	MSM_ISP_MAP_BUF_START_MULTI_PASS_FE,
 	MSM_ISP_CFG_HW_STATE,
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 	MSM_ISP_AHB_CLK_CFG,
+#endif
 	MSM_ISP_UPDATE_FE_FRAME_ID,
 	MSM_ISP_RESTART_FE,
 	MSM_ISP32_REQUEST_STREAM,
@@ -1077,6 +1083,11 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', MSM_ISP_UNMAP_BUF, \
 		struct msm_isp_unmap_buf_req)
 
+#ifdef CONFIG_MACH_XIAOMI_MIDO
+#define VIDIOC_MSM_ISP_AHB_CLK_CFG \
+	_IOWR('V', BASE_VIDIOC_PRIVATE+25, struct msm_isp_ahb_clk_cfg)
+#endif
+
 #define VIDIOC_MSM_ISP_FETCH_ENG_MULTI_PASS_START \
 	_IOWR('V', MSM_ISP_FETCH_ENG_MULTI_PASS_START, \
 		struct msm_vfe_fetch_eng_multi_pass_start)
@@ -1089,8 +1100,10 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', MSM_ISP_CFG_HW_STATE, \
 		struct msm_vfe_axi_stream_cfg_cmd)
 
+#ifndef CONFIG_MACH_XIAOMI_MIDO
 #define VIDIOC_MSM_ISP_AHB_CLK_CFG \
 	_IOWR('V', MSM_ISP_AHB_CLK_CFG, struct msm_isp_ahb_clk_cfg)
+#endif
 
 #define VIDIOC_MSM_ISP_BUF_DONE \
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp32_event_data)

offset1313/android_kernel_xiaomi_mido-4.9

drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -772,7 +772,9 @@ struct vfe_device {
 	uint32_t is_split;
 	uint32_t dual_vfe_enable;
 	unsigned long page_fault_addr;
+#ifndef CONFIG_MACH_XIAOMI_C6
 	uint32_t vfe_hw_limit;
+#endif
 
 	/* Debug variables */
 	int dump_reg;

drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
@@ -2586,8 +2586,10 @@ int msm_vfe47_enable_regulators(struct vfe_device *vfe_dev, int enable)
 int msm_vfe47_get_platform_data(struct vfe_device *vfe_dev)
 {
 	int rc = 0;
+#ifndef CONFIG_MACH_XIAOMI_C6
 	void __iomem *vfe_fuse_base;
 	uint32_t vfe_fuse_base_size;
+#endif
 
 	vfe_dev->vfe_base = msm_camera_get_reg_base(vfe_dev->pdev, "vfe", 0);
 	if (!vfe_dev->vfe_base)
@@ -2612,6 +2614,7 @@ int msm_vfe47_get_platform_data(struct vfe_device *vfe_dev)
 		rc = -ENOMEM;
 		goto get_res_fail;
 	}
+#ifndef CONFIG_MACH_XIAOMI_C6
 	vfe_dev->vfe_hw_limit = 0;
 	vfe_fuse_base = msm_camera_get_reg_base(vfe_dev->pdev,
 					"vfe_fuse", 0);
@@ -2624,6 +2627,7 @@ int msm_vfe47_get_platform_data(struct vfe_device *vfe_dev)
 		msm_camera_put_reg_base(vfe_dev->pdev, vfe_fuse_base,
 				"vfe_fuse", 0);
 	}
+#endif
 	rc = vfe_dev->hw_info->vfe_ops.platform_ops.get_regulators(vfe_dev);
 	if (rc)
 		goto get_regulator_fail;

include/uapi/media/msmb_isp.h
@@ -24,7 +24,10 @@
 
 #define ISP_STATS_STREAM_BIT  0x80000000
 
+#ifndef CONFIG_MACH_XIAOMI_C6
 #define VFE_HW_LIMIT 1
+#endif
+
 #define ISP_KERNEL_STATE 1
 
 struct msm_vfe_cfg_cmd_list;
@@ -504,7 +507,9 @@ enum msm_vfe_reg_cfg_type {
 	VFE_HW_UPDATE_UNLOCK,
 	SET_WM_UB_SIZE,
 	SET_UB_POLICY,
+#ifndef CONFIG_MACH_XIAOMI_C6
 	GET_VFE_HW_LIMIT,
+#endif
 };
 
 struct msm_vfe_cfg_cmd2 {
@@ -961,7 +966,9 @@ enum msm_isp_ioctl_cmd_code {
 	MSM_ISP_FETCH_ENG_MULTI_PASS_START,
 	MSM_ISP_MAP_BUF_START_MULTI_PASS_FE,
 	MSM_ISP_CFG_HW_STATE,
+#ifndef CONFIG_MACH_XIAOMI_C6
 	MSM_ISP_AHB_CLK_CFG,
+#endif
 	MSM_ISP_UPDATE_FE_FRAME_ID,
 	MSM_ISP_RESTART_FE,
 	MSM_ISP32_REQUEST_STREAM,
@@ -1079,6 +1086,11 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', MSM_ISP_UNMAP_BUF, \
 		struct msm_isp_unmap_buf_req)
 
+#ifdef CONFIG_MACH_XIAOMI_C6
+#define VIDIOC_MSM_ISP_AHB_CLK_CFG \
+	_IOWR('V', BASE_VIDIOC_PRIVATE+25, struct msm_isp_ahb_clk_cfg)
+#endif
+
 #define VIDIOC_MSM_ISP_FETCH_ENG_MULTI_PASS_START \
 	_IOWR('V', MSM_ISP_FETCH_ENG_MULTI_PASS_START, \
 		struct msm_vfe_fetch_eng_multi_pass_start)
@@ -1091,8 +1103,10 @@ enum msm_isp_ioctl_cmd_code {
 	_IOWR('V', MSM_ISP_CFG_HW_STATE, \
 		struct msm_vfe_axi_stream_cfg_cmd)
 
+#ifndef CONFIG_MACH_XIAOMI_C6
 #define VIDIOC_MSM_ISP_AHB_CLK_CFG \
 	_IOWR('V', MSM_ISP_AHB_CLK_CFG, struct msm_isp_ahb_clk_cfg)
+#endif
 
 #define VIDIOC_MSM_ISP_BUF_DONE \
 	_IOWR('V', BASE_VIDIOC_PRIVATE+21, struct msm_isp32_event_data)